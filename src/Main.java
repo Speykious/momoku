7d1778a07aa28251a93b44aebdb03744cf0764a1
Speykious <speykious@gmail.com>
2022-01-07 15:33:33 +0100

    Create a Swing Hello World with modern theme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Setup modern look and feel
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new MainWindow());
     }
 }

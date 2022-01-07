9b568a112c058a136bbf192c1f40bc007c75aeae
Speykious <speykious@gmail.com>
2022-01-07 23:10:17 +0100

    Add image files

--- src/GlobalSettings.java
+++ src/GlobalSettings.java
@@ -1,5 +1,7 @@
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Date;
 import java.util.Random;
 import java.util.random.RandomGenerator;
@@ -8,4 +10,11 @@ public final class GlobalSettings {
     public static final Font DEFAULT_FONT = new Font("Roboto Regular", Font.PLAIN, 20);
     public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
     public static final RandomGenerator RANDOM = new Random((new Date()).getTime());
+    public static final File IMAGE_DIRECTORY = new File("");
+    public static final File[] IMAGE_FILES = IMAGE_DIRECTORY.listFiles(new FilenameFilter() {
+        public boolean accept(File dir, String filename) {
+            String name = filename.toLowerCase();
+            return name.endsWith(".png") || name.endsWith(".jpg");
+        }
+    });
 }

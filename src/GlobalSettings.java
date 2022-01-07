3fe584cddaeae31fc142b7516828ab61677b244f
Speykious <speykious@gmail.com>
2022-01-07 17:20:26 +0100

    Default toolkit

--- src/GlobalSettings.java
+++ src/GlobalSettings.java
@@ -1,5 +1,7 @@
 import java.awt.Font;
+import java.awt.Toolkit;
 
 public final class GlobalSettings {
     public static final Font DEFAULT_FONT = new Font("Roboto Regular", Font.PLAIN, 20);
+    public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
 }

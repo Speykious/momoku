0f6bc4203e1da1c587d1547f54b750d42467d76f
Speykious <speykious@gmail.com>
2022-01-07 23:09:59 +0100

    Add default random generator

--- src/GlobalSettings.java
+++ src/GlobalSettings.java
@@ -1,7 +1,11 @@
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.util.Date;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public final class GlobalSettings {
     public static final Font DEFAULT_FONT = new Font("Roboto Regular", Font.PLAIN, 20);
     public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
+    public static final RandomGenerator RANDOM = new Random((new Date()).getTime());
 }

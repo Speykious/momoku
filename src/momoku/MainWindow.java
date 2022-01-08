6d5dc5e85dd120303fbd47d8766b530794604d8c
Speykious <speykious@gmail.com>
2022-01-08 17:24:44 +0100

    Have a main Momoku window for every part

---
+++ src/momoku/MainWindow.java
@@ -0,0 +1,49 @@
+package momoku;
+
+import momoku.components.CenteredWindow;
+
+import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
+
+public class MainWindow extends CenteredWindow {
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+
+    public MainWindow(boolean visible) {
+        super(visible);
+    }
+
+    @Override
+    public void init() {
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        momoku.mainMenu.MainWindow mainMenu = new momoku.mainMenu.MainWindow();
+        mainMenu.setParent(this);
+        mainMenu.run();
+        cardPanel.add(mainMenu.getMainPanel(), "mainMenu");
+
+        momoku.practiceMode.MainWindow practiceMode = new momoku.practiceMode.MainWindow();
+        practiceMode.setParent(this);
+        practiceMode.run();
+        cardPanel.add(practiceMode.getMainPanel(), "practiceMode");
+
+        add(cardPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "exit":
+                System.exit(0);
+                break;
+            default:
+                System.err.println("Showing '" + e.getActionCommand() + "'");
+                cardLayout.show(cardPanel, e.getActionCommand());
+                break;
+        }
+    }
+}

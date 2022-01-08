0bb0262b899d09e3c4d98e0d74e886dc8b084b7a
Speykious <speykious@gmail.com>
2022-01-08 01:01:47 +0100

    Add timer to global settings

--- src/momoku/GlobalSettings.java
+++ src/momoku/GlobalSettings.java
@@ -6,12 +6,15 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Date;
 import java.util.Random;
+import java.util.Timer;
 import java.util.random.RandomGenerator;
 
 public final class GlobalSettings {
     public static final Font DEFAULT_FONT = new Font("Roboto Regular", Font.PLAIN, 20);
     public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
     public static final RandomGenerator RANDOM = new Random((new Date()).getTime());
+    public static final Timer TIMER = new Timer();
+
     public static final File IMAGE_DIRECTORY = new File("images");
     public static final File[] IMAGE_FILES = IMAGE_DIRECTORY.listFiles(new FilenameFilter() {
         public boolean accept(File dir, String filename) {

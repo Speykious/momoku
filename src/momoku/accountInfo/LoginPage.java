899e238b688f23086b1890ede32f7e1de21ec201
Speykious <speykious@gmail.com>
2022-01-10 05:42:12 +0100

    Enhance login page

--- src/momoku/accountInfo/LoginPage.java
+++ src/momoku/accountInfo/LoginPage.java
@@ -1,112 +1,111 @@
 package momoku.accountInfo;
 
+import momoku.GlobalSettings;
 import momoku.components.Screen;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.awt.Font;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.swing.BorderFactory;
-import java.awt.GridLayout;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextField;
-import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
 import javax.swing.JPasswordField;
 
 public class LoginPage extends Screen implements ActionListener {
-    private static final long serialVersionUID = 1L;
-    private JTextField username;
+    private JTextField usernameField;
     private JPasswordField passwordField;
-    private JButton loginButton;
-    private JLabel label;
-    private JPanel contentPane;
+    private JLabel messageLabel;
 
     public LoginPage() {
-        JPanel contentPane = new JPanel();
-        contentPane.setAlignmentX(Component.CENTER_ALIGNMENT);
-        contentPane.setAlignmentY(Component.CENTER_ALIGNMENT);
-        contentPane.setBorder(BorderFactory.createLineBorder(Color.getHSBColor(0f, 0f, .5f), 1));
-        add(contentPane, BorderLayout.CENTER);
+        super();
 
-        JLabel lblNewLabel = new JLabel("Login");
-        lblNewLabel.setForeground(Color.BLACK);
-        lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 46));
-        lblNewLabel.setBounds(423, 13, 273, 93);
-        contentPane.add(lblNewLabel);
+        // Main layout
+        BorderLayout mainLayout = new BorderLayout();
+        mainLayout.setHgap(100);
+        mainLayout.setVgap(100);
 
-        JLabel lblUsername = new JLabel("Username");
-        lblUsername.setBackground(Color.BLACK);
-        lblUsername.setForeground(Color.BLACK);
-        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 31));
-        lblUsername.setBounds(250, 166, 193, 52);
-        contentPane.add(lblUsername);
+        setAlignmentX(Component.CENTER_ALIGNMENT);
+        setAlignmentY(Component.CENTER_ALIGNMENT);
+        setLayout(mainLayout);
 
-        username = new JTextField();
-        username.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        username.setBounds(481, 170, 281, 68);
-        contentPane.add(username);
-        username.setColumns(10);
+        // Header
+        JLabel header = new JLabel("Log in / Sign in", SwingConstants.CENTER);
+        header.setFont(GlobalSettings.HEADER_FONT);
+        add(header, BorderLayout.NORTH);
 
-        JLabel lblPassword = new JLabel("Password");
-        lblPassword.setForeground(Color.BLACK);
-        lblPassword.setBackground(Color.CYAN);
-        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 31));
-        lblPassword.setBounds(250, 286, 193, 52);
-        contentPane.add(lblPassword);
+        // Center
+        JPanel formPanel = new JPanel();
+        GridLayout formLayout = new GridLayout(0, 2);
+        formLayout.setHgap(20);
+        formLayout.setVgap(30);
+        formPanel.setLayout(formLayout);
+        formPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        formPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        Border formBorder = BorderFactory.createCompoundBorder(
+            BorderFactory.createLineBorder(Color.getHSBColor(0f, 0f, .5f), 1),
+            new EmptyBorder(100, 150, 100, 150));
+        formPanel.setBorder(formBorder);
+
+        JLabel usernameLabel = new JLabel("Username", SwingConstants.CENTER);
+        formPanel.add(usernameLabel);
+
+        usernameField = new JTextField();
+        usernameField.setActionCommand("clearMessage");
+        usernameField.addActionListener(this);
+        formPanel.add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password", SwingConstants.CENTER);
+        formPanel.add(passwordLabel);
 
         passwordField = new JPasswordField();
-        passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        passwordField.setBounds(481, 286, 281, 68);
-        contentPane.add(passwordField);
+        passwordField.setActionCommand("clearMessage");
+        passwordField.addActionListener(this);
+        formPanel.add(passwordField);
 
-        loginButton = new JButton("Login");
-        loginButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        formPanel.add(new Box(BoxLayout.X_AXIS));
+        formPanel.add(new Box(BoxLayout.X_AXIS));
+
+        JButton loginButton = new JButton("Log in");
         loginButton.setActionCommand("login");
         loginButton.addActionListener(this);
-        contentPane.add(loginButton);
+        formPanel.add(loginButton);
 
-        label = new JLabel("");
-        label.setBounds(0, 0, 1008, 562);
-        contentPane.add(label);
+        JButton signinButton = new JButton("Sign in");
+        signinButton.setActionCommand("register");
+        signinButton.addActionListener(this);
+        formPanel.add(signinButton);
 
-        /*try {
-			Socket s = new Socket("localhost",3000);
-			MultiClients t = new MultiClients(s,this);
-			t.start();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		};*/
+        add(formPanel, BorderLayout.CENTER);
+
+        messageLabel = new JLabel(" ", SwingConstants.CENTER);
+        messageLabel.setForeground(Color.getHSBColor(0f, .7f, .8f));
+        add(messageLabel, BorderLayout.SOUTH);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
-            case "login":
-                //BDD
-            
-            case "register":
-                //link to register page
+            case "clearMessage":
+                messageLabel.setText(" ");
                 break;
-            }
-        
+            case "login":
+                messageLabel.setText("Will login shortly");
+                break;
+            case "register":
+                messageLabel.setText("Will register shortly");
+                break;
+        }
     }
-}
-    
 }
